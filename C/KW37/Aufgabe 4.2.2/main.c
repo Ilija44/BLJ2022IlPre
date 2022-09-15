3fa70cdfda13e94497fda4bfde3a95aeaadced69
ilija44 <ilija.predolac@noseryoung.com>
2022-09-15 15:42:20 +0200

    Added KW37 Files

---
+++ C/KW37/Aufgabe 4.2.2/main.c
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: ilija
+ *
+ * Created on 15. September 2022, 13:26
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+double p = 3.14;
+double U;
+int r = 5;
+U = p*2*r;
+
+printf(" Umfang = %.2f ",U);
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ C/KW37/Aufgabe 4/main.c
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: ilija
+ *
+ * Created on 15. September 2022, 12:07
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    
+    int a = 1000;
+    
+    
+    printf("%d \t%0x \t%o", a, a,a);
+    
+
+    return (EXIT_SUCCESS);
+}
+
