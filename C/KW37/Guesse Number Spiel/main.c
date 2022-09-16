bc0d80dd3b51a1261ebe33b41aaf3e0ea765d13f
ilija44 <ilija.predolac@noseryoung.com>
2022-09-16 14:56:56 +0200

    Nummer Projekt hochgeladen

---
+++ C/KW37/Guesse Number Spiel/main.c
@@ -0,0 +1,33 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   int guessedNum;
+   srand(time(NULL));
+    int ranNum = rand() % 20;
+    
+    
+    printf("🆆🅸🅻🅻🅺🅾🅼🅼🅴🅽 🆉🆄🅼 🆉🅰🅷🅻🅴🅽 🅴🆁🆁🅰🆃🅴🅽 🆂🅿🅸🅴🅻\n");
+    printf("Geben sie eine Zahl an:");
+    scanf("%d", &guessedNum);
+    while(1){
+        if (guessedNum > ranNum){
+        
+            printf("Die Zahl ist zu hoch geben sie eine tiefere an:");
+            scanf ("%d", &guessedNum);
+        }
+        
+        if (guessedNum < ranNum){
+            printf("Die Zahl ist zu tief geben sie eine höhere an:");
+            scanf ("%d" , &guessedNum);
+    }
+        if (guessedNum == ranNum){
+          
+            printf("Sie haben die richtige Zahl erraten");
+            break;
+          }
+    }
+
+
+    return 0;
+}
