31a68629dd385bd61483de05f6daa5c590d4be4c
Ilija44 <ilija.predolac@noseryoung.com>
2022-09-30 12:13:22 +0200

    Bubblesort Zahlenordner erstellt

---
+++ C/KW39/Bubblesort/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define NUM_NUMBERS 8
+
+int main() {
+
+
+    int values [] = {2,6, 3,70, 23, 9, 12, 44};
+
+    for (int i = 0; NUM_NUMBERS > i ; i++) {
+
+
+        for (int i = 0; NUM_NUMBERS > i; i++) {
+            if (values[i] > values[i + 1]) {
+
+                int temp = values[i];
+
+                values[i] = values[i + 1];
+
+                values[i + 1] = temp;
+            }
+        }
+    }
+    for (int i = 0; NUM_NUMBERS > i ; i++){
+        printf("%d \n", values[i]);
+    }
+
+
+    return 0;
+}
