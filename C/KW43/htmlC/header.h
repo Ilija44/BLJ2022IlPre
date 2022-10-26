8bfc9ad3085d90d118a6bee23157ad4ba4d783cc
Ilija44 <ilija.predolac@noseryoung.com>
2022-10-26 09:58:23 +0200

    create header.h

---
+++ C/KW43/htmlC/header.h
@@ -0,0 +1,16 @@
+#ifndef HTMLC_HEADER_H
+#define HTMLC_HEADER_H
+
+void console(
+        int columns,
+        int rows,
+        int structure
+);
+
+void externalfile(
+        int columns,
+        int rows,
+        int structure
+);
+
+#endif //HTMLC_HEADER_H
