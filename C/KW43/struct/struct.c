b048b65f9483ddf0da9fdeca18c1125282f7d95d
Ilija44 <ilija.predolac@noseryoung.com>
2022-10-28 16:43:10 +0200

    updated struct programm

--- C/KW43/struct/struct.c
+++ C/KW43/struct/struct.c
@@ -15,21 +15,21 @@ void printSong(char title[20], char singer[20], int published){
 
 int main() {
 
-     char title[20] = "la la la la";
-     char singer[20] = "Shakira";
-     int published = 2017;
+    char title[20] = "la la la la";
+    char singer[20] = "Shakira";
+    int published = 2017;
 
-     printSong(title, singer, published);
+    printSong(title, singer, published);
 
-     TSong song = {0};
-     song.published = 2017;
-     strcpy(song.title, "la la la la");
-     strcpy(song.singer, "Shakira");
+    TSong song = {0};
+    song.published = 2017;
+    strcpy(song.title, "la la la la");
+    strcpy(song.singer, "Shakira");
 
 
 
-     TSong *p_song = &song;
-     printf("Title: %s \n", (*p_song).title);
+    TSong *p_song = &song;
+    printf("Title: %s \n", (*p_song).title);
 
 
     return 0;
