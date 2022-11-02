a6c3eaf228ce1d9b2138c718aae096ae95906b9e
Ilija44 <ilija.predolac@noseryoung.com>
2022-11-02 12:12:57 +0100

    Add Inputhandler to RandomNum program
    Its not possible anymore to type in a number bigger than 1000 and lower than 1. Also letters are not possible.

--- C/KW44/randomNum/main.c
+++ C/KW44/randomNum/main.c
@@ -10,25 +10,30 @@ int main() {
 
     do {
         printf("How many random numbers do you want to generate: \n");
-     
+        fflush(stdin);
         scanf("%d", &answer);
 
-
-        getRandomNums(answer);
-
-        return 0;
-    }
-
-    int getRandomNums(int answer) {
-
-        srand(time(NULL));
-
-        int *nums = malloc(answer * sizeof(int));
-
-        if (nums == NULL) exit(1);
-
-        for (int n = 0; n < answer; n++) {
-            printf("%d   ", rand() % 100 + 1);
+        if (answer < 1 || answer > 1000) {
+            printf("Enter please a number between 1-1000 \n");
         }
-        return nums;
+
+    } while (answer < 1 || answer > 10000);
+
+    getRandomNums(answer);
+
+    return 0;
+}
+
+int getRandomNums(int answer) {
+
+    srand(time(NULL));
+
+    int *nums = malloc(answer * sizeof(int));
+
+    if (nums == NULL) exit(1);
+
+    for (int n = 0; n < answer; n++) {
+        printf("%d   ", rand() % 100 + 1);
     }
+    return nums;
+}
