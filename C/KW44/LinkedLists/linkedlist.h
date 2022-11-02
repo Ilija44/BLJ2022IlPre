3b25f2bb90dd22385f985aba61542b13ac964dd4
Ilija44 <ilija.predolac@noseryoung.com>
2022-11-02 16:12:07 +0100

    created all source files and added more Nodes

---
+++ C/KW44/LinkedLists/linkedlist.h
@@ -0,0 +1,5 @@
+
+#ifndef LINKEDLISTS_LINKEDLIST_H
+#define LINKEDLISTS_LINKEDLIST_H
+
+#endif
---
+++ C/KW44/LinkedLists/linkedlistsource.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct Node {
+    int data;
+    struct Node* next;
+} TNode;
+
+void main() {
+
+    struct Node *head = NULL;
+    struct Node *second = NULL;
+    struct Node *third = NULL;
+
+    head = (struct Node *) malloc(sizeof(struct Node));
+    second = (struct node *) malloc(sizeof(struct Node));
+    third = (struct node *) malloc(sizeof(struct Node));
+
+    
+
+}
