61e54359ad392acd296fdb600b40fd5922ed9658
Ilija44 <ilija.predolac@noseryoung.com>
2022-11-03 09:47:00 +0100

    add New Project because of Errors in the other

---
+++ C/KW44/linkedlist2/linkedlist.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef struct Node{
+    int data;
+    struct Node *next;
+} TNode;
+
+int *StartNodes() {
+
+    struct Node *head = NULL;
+    struct Node *second = NULL;
+    struct Node *third = NULL;
+
+    head = (struct Node *) malloc(sizeof(struct Node));
+    second = (struct Node *) malloc(sizeof(struct Node));
+    third = (struct Node *) malloc(sizeof(struct Node));
+
+    head->data = 1;
+    head->next = second;
+
+    second->data = 2;
+    second->next = third;
+
+    third->data = 3;
+    third->next = NULL;
+
+    return (int *) head;
+}
+
+void printList(struct Node *n) {
+    while (n != NULL) {
+        printf(" %d ", n->data);
+        n = n->next;
+    }
+}
---
+++ C/KW44/linkedlist2/linkedlist.h
@@ -0,0 +1,9 @@
+
+
+#ifndef LINKEDLIST2
+#define LINKEDLIST2
+int *StartNodes();
+
+void printList(head);
+
+#endif
