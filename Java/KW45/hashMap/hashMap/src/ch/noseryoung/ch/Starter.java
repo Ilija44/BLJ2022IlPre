9489a66def7e24923e2831aa0dd7ef5c5bc346ea
Ilija44 <ilija.predolac@noseryoung.com>
2022-11-16 11:44:42 +0100

    add testing from morse to text

--- Java/KW45/hashMap/hashMap/src/ch/noseryoung/ch/Starter.java
+++ Java/KW45/hashMap/hashMap/src/ch/noseryoung/ch/Starter.java
@@ -1,20 +1,23 @@
 package ch.noseryoung.ch;
 
-import java.util.HashMap;
 
 public class Starter {
 
 
     public static void main(String[] args) {
         translator translator2 = new translator();
-        String Input = ("Hallo, wie gehts?");
+        String Input = ("hallo");
         String temp;
         char[] CharrArray = Input.toCharArray();
-
 
         for (char c : CharrArray) {
             temp = Character.toString(c).toLowerCase();
             System.out.print(translator2.getMorseTable().get(temp));
+        }
+
+        for (char c : CharrArray) {
+            temp = Character.toString(c).toLowerCase();
+            System.out.print(translator2.get2Morse().get(temp));
         }
     }
 
--- Java/KW45/hashMap/hashMap/src/ch/noseryoung/ch/translator.java
+++ Java/KW45/hashMap/hashMap/src/ch/noseryoung/ch/translator.java
@@ -1,9 +1,10 @@
 package ch.noseryoung.ch;
 
 import java.util.HashMap;
-import java.util.Scanner;
+
 public class translator {
     public HashMap<String, String> morseTable = new HashMap<>();
+    public HashMap<String, String> getMorse = new HashMap<>();
     public translator() {
         morseTable.put("a", ".-");
         morseTable.put("b", "-...");
@@ -30,14 +31,38 @@ public class translator {
         morseTable.put("x", "-..-");
         morseTable.put("y", "-.--");
         morseTable.put("z", "--..");
+
+        morseTable.put("1", ".----");
+        morseTable.put("2", "..---");
+        morseTable.put("3", "...--");
+        morseTable.put("4", "....-");
+        morseTable.put("5", ".....");
+        morseTable.put("6", "-....");
+        morseTable.put("7", "--...");
+        morseTable.put("8", "---..");
+        morseTable.put("9", "----.");
+        morseTable.put("0", "-----");
+
         morseTable.put(".", ".-.-.-");
         morseTable.put("?", "..--..");
         morseTable.put(",", "--..--");
         morseTable.put(" ", "/");
 
     }
-
     public HashMap<String, String> getMorseTable() {
         return morseTable;
+    }
+
+    public HashMap<String, String> get2Morse() {
+        return getMorse;
+    }
+
+    public HashMap<String, String> getMorse(){
+
+
+        for(String key: morseTable.keySet()) {
+            getMorse.put(morseTable.get(key), key);
+        }
+        return getMorse;
     }
 }
