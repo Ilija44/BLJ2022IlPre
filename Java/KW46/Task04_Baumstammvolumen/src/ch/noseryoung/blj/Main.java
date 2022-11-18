cc46f61e18f5a24877836332fe0871f2d7a9e88a
Ilija44 <ilija.predolac@noseryoung.com>
2022-11-18 14:52:00 +0100

    add Task04_Baumstammvolumen

---
+++ Java/KW46/Task04_Baumstammvolumen/src/ch/noseryoung/blj/Main.java
@@ -0,0 +1,15 @@
+package ch.noseryoung.blj;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int L = 15;
+        int D = 32;
+
+
+        double result = Math.PI / 4 * D * D * L / 10000;
+
+        System.out.println(result);
+
+    }
+}
