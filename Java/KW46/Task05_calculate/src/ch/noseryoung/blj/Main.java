cd449bd194c25f6140bd40bea4e9b5a0aa18ffbb
Ilija44 <ilija.predolac@noseryoung.com>
2022-11-18 15:31:21 +0100

    add task05_CalculateSum

---
+++ Java/KW46/Task05_calculate/src/ch/noseryoung/blj/Main.java
@@ -0,0 +1,16 @@
+package ch.noseryoung.blj;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        int n = 100;
+        int result = 0;
+
+        for (int i = 1; i <= n; i++) {
+            result = result + i;
+
+        }
+        System.out.println("The Result is: " + result);
+    }
+}
