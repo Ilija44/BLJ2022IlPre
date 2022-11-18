66074f26174696ddda4bf49550e703bfa3285e43
Ilija44 <ilija.predolac@noseryoung.com>
2022-11-18 14:36:56 +0100

    Add Task03

---
+++ Java/KW46/Task03_stoffwechselrate/src/ch/noseryoung/ch/Main.java
@@ -0,0 +1,20 @@
+package ch.noseryoung.ch;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        int m = 90;
+        int l = 160;
+        int t = 45;
+
+        double Men = 66.47 + 13.7 * m + 5 * l - 6.8 * t;
+        double Women = 655.1 + 9.6 * m + 1.8 * l - 4.7 * t;
+
+
+        System.out.println("Men calories: " + Men);
+        System.out.println("Women calories: " + Women);
+
+
+    }
+}
