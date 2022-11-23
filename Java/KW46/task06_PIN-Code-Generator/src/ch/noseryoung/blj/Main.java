47111eb5c4399f90370b4b661e5d149e2cb81d75
Ilija44 <ilija.predolac@noseryoung.com>
2022-11-23 09:02:01 +0100

    add task06_PIN-Code-Generator

---
+++ Java/KW46/task06_PIN-Code-Generator/src/ch/noseryoung/blj/Main.java
@@ -0,0 +1,24 @@
+package ch.noseryoung.blj;
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+        for (int i = 0; i <= 9; i++) {
+            System.out.println("000" + i);
+
+        }
+        for (int j = 10; j <= 99; j++) {
+            System.out.println("00" + j);
+
+        }
+        for (int j = 100; j <= 999; j++) {
+            System.out.println("0" + j);
+
+        }
+        for (int j = 1000; j <= 9999; j++) {
+            System.out.println(j);
+
+        }
+    }
+}
