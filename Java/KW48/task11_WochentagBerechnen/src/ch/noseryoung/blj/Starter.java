f2f9756299dad2ef4df63eda4b36d457478ea9b0
Ilija44 <ilija.predolac@noseryoung.com>
2022-12-07 14:48:46 +0100

    add task11_Weekday check

--- Java/KW48/task11_WochentagBerechnen/src/ch/noseryoung/blj/Starter.java
+++ Java/KW48/task11_WochentagBerechnen/src/ch/noseryoung/blj/Starter.java
@@ -1,14 +1,14 @@
+
 package ch.noseryoung.blj;
 
 public class Starter {
-
-
-    public static String CheeckWeekDay(int day, int month, int year){
+    public static String CheckWeekDay(int day, int month, int year) {
         int yearYY = year % 100;
         int YYyear = year / 100;
         int calculate = yearYY + (yearYY / 4);
         int temp = 1;
         String WeekDay = "";
+
 
         if (month == 1 || month == 10) {
             temp = 1;
@@ -28,8 +28,53 @@ public class Starter {
 
         calculate += temp;
         calculate += day;
+
+        if (YYyear == 18) {
+            calculate += 2;
+        } else if (YYyear == 20) {
+            calculate += 6;
+        } else if (YYyear == 21) {
+            calculate += 4;
+        }
+
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    if (month == 1 || month == 2) {
+                        calculate++;
+                    }
+                }
+            }
+        }
+
+        int result = calculate % 7;
+        if (result == 2) {
+            WeekDay = "Monday";
+        } else if (result == 3) {
+            WeekDay = "Tuesday";
+        } else if (result == 4) {
+            WeekDay = "Wednesday";
+        } else if (result == 5) {
+            WeekDay = "Thursday";
+        } else if (result == 6) {
+            WeekDay = "Friday";
+        } else if (result == 7) {
+            WeekDay = "Saturday";
+        } else if (result == 8) {
+            WeekDay = "Sunday";
+        } else {
+            WeekDay = "Something is wrong";
+        }
+        return WeekDay;
     }
+
     public static void main(String[] args) {
+        int day = 7;
+        int month = 12;
+        int year = 2022;
+
+        String result = CheckWeekDay(day, month, year);
+        System.out.println(result);
 
     }
 }
