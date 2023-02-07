ab544a66a1aa479238378ce6ebedb5b399dace6d
Ilija44 <ilija.predolac@noseryoung.com>
2023-02-07 08:45:40 +0100

    Commiting code for syntax Error Example

---
+++ Java/KW06/SyntaxSchnuppertag/src/ch/noeryoung/blj/Main.java
@@ -0,0 +1,13 @@
+package ch.noeryoung.blj;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int 0 = 0;
+        boolean zwei = 4;
+        double a1 = "Hallo";
+
+        System.out.println(a1, zwei)
+
+    }
+}
