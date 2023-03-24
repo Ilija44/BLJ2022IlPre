3f655ca9b2a6b7ceaa42d3350f23277060ecdef2
Ilija44 <ilija.predolac@noseryoung.com>
2023-03-24 15:37:16 +0100

    Comitting testcases from TriangleValidator

---
+++ Java/KW12/GeometryChallenge/GeometryChallenge/src/test/java/ch/noseryoung/test/TriangleValidatorTests.java
@@ -0,0 +1,88 @@
+package ch.noseryoung.test;
+
+import ch.noseryoung.main.Triangle;
+import ch.noseryoung.main.TriangleValidator;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class TriangleValidatorTests {
+
+    @Test
+    public void isEquilateral_EquilateralTriangle_ReturnsTrue() {
+
+        TriangleValidator testee = new TriangleValidator();
+
+        Triangle t = new Triangle(3, 3, 3);
+
+        boolean expectedResult = true;
+
+        boolean result = testee.isEquilateral(t);
+
+        assertEquals(expectedResult, result);
+    }
+
+
+
+    @Test
+    public void isEquilateral_EquilateralTriangle2_ReturnsTrue(){
+
+        TriangleValidator testee2 = new TriangleValidator();
+
+        Triangle t = new Triangle(3.3, 3.3, 3.3);
+
+        boolean expectedResult = true;
+
+        boolean result = testee2.isEquilateral(t);
+
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    public void IsEquilateral_WithNegativeSides_ReturnsTrue(){
+
+        TriangleValidator testee3 = new TriangleValidator();
+
+        Triangle t = new Triangle(-3,  -3, -3);
+
+        boolean expectedResult = true;
+
+        boolean result = testee3.isEquilateral(t);
+
+        assertEquals(expectedResult, result);
+    }
+
+
+
+    @Test
+    public void IsIsosceles_WithCorrectNumbers_ReturnsTrue(){
+
+        TriangleValidator testee4 = new TriangleValidator();
+
+        Triangle t = new Triangle(2,  2, 3);
+
+        boolean expectedResult = true;
+
+        boolean result = testee4.isIsosceles(t);
+
+        assertEquals(expectedResult, result);
+    }
+
+
+
+    @Test
+    public void IsIsosceles_With1Number0_ReturnsFalse(){
+
+        TriangleValidator testee4 = new TriangleValidator();
+
+        Triangle t = new Triangle(2,  2, 3);
+
+        boolean expectedResult = true;
+
+        boolean result = testee4.isIsosceles(t);
+
+        assertEquals(expectedResult, result);
+    }
+
+}
